4a4a5430a24ddce6bde7dec2eff45fce9910fd9a
Nastya Luna <n.vzdor@gmail.com>
2019-10-17 18:12:38 +0300

    Home module added. react-scripts updated.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import Home from './Home';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        App
+        <Home/>
       </div>
     );
   }
